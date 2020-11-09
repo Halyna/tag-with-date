f62d4e10ecca1526220ffecfbc778d6932f0b472
Halyna Rubashko <halyna.rubashko@gmail.com>
2020-11-09 16:41:31 +1030

    Auto incrementing revision number

--- index.js
+++ index.js
@@ -1,44 +1,64 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const github = require('@actions/github')
 
 async function run() {
     try {
-        const tagValue = core.getInput('tag-value');
-        const revision = core.getInput('revision-number');
-        console.log(`Base tag ${tagValue}`);
-        const time = (new Date()).toISOString().slice(0, 10).replace(/-/g, "");
-        const finalTagValue = `${tagValue}/${time}.${revision}`
-        core.setOutput("final-tag-value", finalTagValue);
-        // const payload = JSON.stringify(github.context.payload, undefined, 2)
-        // console.log("Payload " + payload);
-        console.log(`Final tag to push: ${finalTagValue}`);
+        const tagValue = core.getInput('tag-value') || "test"
+        let revision = core.getInput('revision-number') || 1
+        console.log(`Base tag ${tagValue}`)
+        const time = (new Date()).toISOString().slice(0, 10).replace(/-/g, "")
+        let finalTagValue = `${tagValue}/${time}.${revision}`
 
-        console.log(`Getting vars`);
-        const token = core.getInput('github_token');
-        const sha = process.env.GITHUB_SHA || '';
-        const owner = github.context.repo.owner;
-        const repo = github.context.repo.repo;
-        console.log(`Token ${token}`);
-        console.log(`Repo ${JSON.stringify(repo)}`);
+        console.log(`Getting vars`)
+        let token = core.getInput('github_token')
+        if (!token) {
+            token = "token"
+        }
+        console.log(`Token ${token}`)
+        const sha = process.env.GITHUB_SHA || ''
+        let owner
+        let repo
+        if (!process.env.GITHUB_REPOSITORY) {
+            console.log("empty")
+            owner = 'mightykingdom'
+            repo = 'MKNetV2'
+        } else {
+            owner = github.context.repo.owner
+            repo = github.context.repo.repo
+        }
 
-        console.log(`Creating octokit`);
-        const octokit = github.getOctokit(token);
+        console.log(`Repo ${JSON.stringify(repo)}`)
 
-        // console.log(`Getting existing tag`);
-        // const tag = await octokit.git.getTag({owner, repo, finalTagValue});
-        //
-        // console.log(`Existing tag ${tag}`);
+        console.log(`Creating octokit`)
+        const octokit = github.getOctokit(token)
 
-        console.log(`Pushing new tag to the repo`);
+
+        var existingTags = await octokit.repos.listTags({
+            owner,
+            repo,
+            per_page: 100
+        });
+
+        let filtered = existingTags.data.filter(item => item.name === finalTagValue)
+        while (filtered.length > 0) {
+            revision += 1
+            finalTagValue = `${tagValue}/${time}.${revision}`
+            filtered = existingTags.data.filter(item => item.name === finalTagValue)
+        }
+
+        console.log(`Final tag to push: ${finalTagValue}`)
+        core.setOutput("final-tag-value", finalTagValue)
+
+        console.log(`Pushing new tag to the repo`)
         await octokit.git.createRef({
             owner,
             repo,
             ref: `refs/tags/${finalTagValue}`,
             sha: sha,
-        });
+        })
     } catch (error) {
-        core.setFailed(error.message);
+        core.setFailed(error.message)
     }
 }
 
-run();
+run()
